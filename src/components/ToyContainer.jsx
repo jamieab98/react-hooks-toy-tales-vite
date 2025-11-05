bb7b73a5524714f989ea31c2b0786c7aba8c82ce
Jamie Bryson <jamieab98@gmail.com>
2025-11-05 16:38:26 -0500

    got it so a ToyCard component is loaded every for every object in data.

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ToyCard from "./ToyCard";
+import { useEffect, useState } from "react";
 
 function ToyContainer() {
+
+  const [toysData, setToysData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/toys')
+    .then((r) => r.json())
+    .then((data) => setToysData(data))
+  }, [])
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {toysData.map((toyData) => (
+        <ToyCard/>
+      ) )}
+    </div>
   );
 }
 
